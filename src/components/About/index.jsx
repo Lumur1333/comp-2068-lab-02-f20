2b226fa482587573a2fad58802c5f733a10b2ede
Lumur1333 <200304037@student.georgianc.on.ca>
2020-10-20 20:06:26 -0400

    final push

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,10 +3,10 @@ import Header from '../shared/Header';
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Header title="About"/>
       <p>I am a huge fan of studio gibli films! My favourite film is princess mononoke, probably followed by spirited away, and howl's moving castle after that but they are all good films.</p>
-    </div>
+    </>
   );
 }
  
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Header from '../shared/Header';
+import Styles from './styles.css';
 
 const Home = () => {
   return (
     <>
       <Header title="Home"/>
+      
       <button>
         <Link to="/about">About</Link>
       </button>
+      <div>
+      <iframe src="https://i.pinimg.com/originals/3c/49/e5/3c49e5ab5eca9b57f3bba2e98ebd4fe9.jpg" width="500" height="600" frameBorder="0" allowFullScreen></iframe>
+      </div>
     </>
   );
 }
